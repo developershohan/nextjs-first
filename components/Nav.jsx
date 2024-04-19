851bc2628537063fe5210e60103544eb9f45ce1f
Developer Shohan <dev.shohanur@gmail.com>
2024-04-19 16:58:20 +0600

    navbar completed with out auth

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -3,80 +3,124 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
-import { signIn, signOut, useSession, getProvider } from "next-auth/react";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-    const isUserLoggedIn = true;
+  const isUserLoggedIn = true;
 
-    //   define providers
-    const [providers, setProviders] = useState(null)
-    useEffect(() => {
-        const setProviders = async () => {
-            const response = await getProvider()
-            setProviders(response)
-        }
-        setProviders()
+  //   define providers
+  const [providers, setProviders] = useState(null);
+  const [toggleDropdown, setToggleDropdown] = useState(false);
 
-    }, [])
+  useEffect(() => {
+    const setProviders = async () => {
+      const response = await getProviders();
+      setProviders(response);
+    };
+    setProviders();
+  }, []);
 
+  return (
+    //nav wrapper
+    <nav className=" w-full mb-16 pt-3 flex-between">
+      {/* logo */}
+      <Link href="/" className=" flex gap-3">
+        <Image
+          src="/assets/images/logo.svg"
+          alt=" site logo"
+          width={30}
+          height={30}
+        />
+        {/* logo text */}
+        <p className=" logo_text">shohan</p>
+      </Link>
 
-
-    return (
-        //nav wrapper
-        <nav className=" w-full mb-16 pt-3 flex-between">
-            {/* logo */}
-            <Link href="/" className=" flex gap-3">
-                <Image
-                    src="/assets/images/logo.svg"
-                    alt=" site logo"
-                    width={30}
-                    height={30}
-                />
-                {/* logo text */}
-                <p className=" logo_text">shohan</p>
+      {/* desktop navigation */}
+      <div className=" sm:flex hidden">
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className=" black_btn">
+              Create Post
             </Link>
+            <button type="button" onClick={signOut} className=" outline_btn">
+              Sign out
+            </button>
 
-            {/* desktop navigation */}
-            <div className=" sm:flex hidden">
-                {isUserLoggedIn ? (
-                    <div className="flex gap-3 md:gap-5">
-                        <Link href="/create-prompt" className=" black_btn">
-                            Create Post
-                        </Link>
-                        <button type="button" onClick={signOut} className=" outline_btn">
-                            Sign out
-                        </button>
+            {/* avatar */}
+            <Link href="/profile">
+              <Image
+                src="/assets/images/logo.svg"
+                alt=" Profile"
+                width={37}
+                height={37}
+                className=" rounded-full"
+              />
+            </Link>
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className=" black_btn">
+                  Sign In
+                </button>
+              ))}
+          </>
+        )}
+      </div>
+      {/* mobile navigation */}
+      <div className="flex relative sm:hidden">
+        {isUserLoggedIn ? (
+          <div className="flex">
+            <Image
+              src="/assets/images/logo.svg"
+              alt=" Profile"
+              width={37}
+              height={37}
+              className=" rounded-full"
+              onClick={() => setToggleDropdown((prev) => !prev)}
+            />
 
-                        {/* avatar */}
-                        <Link href="/profile">
-                            <Image
-                                src="/assets/images/logo.svg"
-                                alt=" Profile"
-                                width={37}
-                                height={37}
-
-                                className=" rounded-full"
-                            />
-                        </Link>
-                    </div>
-                ) : (
-                    <>
-                        {providers && 
-                        
-                        Object.values(providers).map((providers)=>(
-                            <button type="button" key={provider.name}
-                            
-                            onClick={()=>signIn(provider.id)} className=" black_btn">
-                            Sign In
-                        </button>
-                        ))
-                        }
-                        </>
-                )}
-            </div>
-            {/* mobile navigation */}
-        </nav>
-    );
+            {toggleDropdown && (
+              <div className="dropdown">
+                <Link
+                  href={"/profile"}
+                  className=" dropdown_link "
+                  onClick={() => setToggleDropdown(false)}>
+                  profile
+                </Link>
+                <Link href={"create-prompt "}
+                 className=" dropdown_link "
+                  onClick={() => setToggleDropdown(false)}>
+                  Create Prompt 
+                </Link>
+                <button className=" black_btn w-full mt-5">
+                    Sign Out
+                </button>
+              </div>
+            )}
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className=" black_btn">
+                  Sign In
+                </button>
+              ))}
+          </>
+        )}
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
