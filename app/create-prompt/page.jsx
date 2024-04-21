b8dae64e1c19a2ac87cc9942db7e86e14e83e7ff
Developer Shohan <dev.shohanur@gmail.com>
2024-04-21 23:18:53 +0600

    prompt post added

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+"use client";
 
-const createPrompt = () => {
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import Form from "@components/Form";
+import { useRouter } from "next/navigation";
+
+const CreatePrompt = () => {
+  const {data: session} = useSession();
+  const router = useRouter();
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    prompt: "",
+    tag: "",
+  });
+  const createPrompt = async (e) => {
+    e.preventDefault();
+
+    setSubmitting(true);
+
+    try {
+      const res = await fetch("api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          tag: post.tag,
+          userId: session?.user.id,
+        }),
+      });
+      if (res.ok) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
   return (
-    <div>createPrompt</div>
-  )
-}
+    <Form
+      type="Create"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createPrompt}
+    />
+  );
+};
 
-export default createPrompt
+export default CreatePrompt;
--- components/Form.jsx
+++ components/Form.jsx
@@ -1,8 +1,52 @@
+import Link from "next/link";
 
-const Form = () => {
+const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
   return (
-    <div>Form</div>
-  )
-}
+    <section className=" w-full max-w-full">
+      <h1 className="head_text text-left">
+        <span className="blue_gradient">{type} Post</span>
+      </h1>
+      <form onSubmit={handleSubmit} className="mt-10 flex w-full max-w-2xl flex-col gap-7 glassmorphism">
+        <label>
+          <span className=" font-satoshi font-semibold text-base text-gray-700">
+            Your prompt
+          </span>
+          <textarea
+            value={post.prompt}
+            onChange={(e) => setPost({ ...post, prompt: e.target.value })}
+            placeholder="Write your prompt here..."
+            className=" form_textarea"
+          />
+        </label>
 
-export default Form
+        <label>
+          <span className=" font-satoshi font-semibold text-base text-gray-700">
+            Tag <span className=" font-normal">(#product, #idea) </span>
+          </span>
+
+          <input
+            value={post.tag}
+            onChange={(e) => setPost({ ...post, tag: e.target.value })}
+            placeholder="Write your tag here..."
+            className=" form_input"
+          />
+        </label>
+
+        <div className="flex-end mx-3 mb-5 gap-4">
+          <Link href="/" className=" text-gray-500 text-sm">
+            Cancel
+          </Link>
+          <button
+            type="submit"
+         
+            disabled={submitting}
+            className=" bg-primary-orange rounded-full text-white px-5 py-1.5 text-sm">
+            {submitting ? `${type}...` : type}
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default Form;
